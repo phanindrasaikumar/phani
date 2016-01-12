260ef2636b109e3a9341ffa79bcfe7e00d7a6053
phani <phanindrassaikumar.k@gmail.com>
2016-01-12 14:40:58 +0530

    initial-release

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phani::Application.config.secret_token = '148409590cd0f2f7af7012f7b9bac7a0965b6f38d9e8f1d70731a4629dc5862991a8497186a738896ec8e73e3356023079577fb176026db0d5ea0101e188a650'
